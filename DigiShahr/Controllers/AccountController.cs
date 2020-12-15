64d039edeb4d5efec0bf815206b8fc398ea934d7
nazila bagheri <nazila.bagheri93@gmail.com>
2020-12-15 10:40:42 +0330

    layout and createAccount and packages view pages

---
+++ DigiShahr/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiShahr.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateAccount()
+        {
+            return View();
+        }
+        public IActionResult BuyPackage()
+        {
+            return View();
+        }
+        public IActionResult CreateStore()
+        {
+            return View();
+        }
+    }
+}
